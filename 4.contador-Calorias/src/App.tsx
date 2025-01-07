c8a41039c5ee8630a1f8d1b0040dfbd75f09fee4
daniel mena <dmena044@gmail.com>
2025-01-06 22:30:18 -0600

    se elimino proyectos que deben estan en develop

--- 4.contador-Calorias/src/App.tsx
+++ 4.contador-Calorias/src/App.tsx
@@ -1,23 +1,21 @@
-import { useState } from 'react'
 import './App.css'
-import { Form } from './components/Form.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <header className='bg-slate-500 py-3'>
-        <div className='max-w-4xl mx-auto flex justify-between'>
-            <h1 className='text-center text-lg font-bold uppercase text-white'>Contador de Calorias</h1>
+    <header className='bg-indigo-500 py-3'>
+        <div className="max-w-4xl mx-auto flex justify-between">
+            <h1 className='text-center text-lg font-bold text-white uppercase'>
+                Contador de Calorias
+            </h1>
         </div>
     </header>
 
-    <section className='bg-orange-300 py-20 px-5'>
-        <div className='max-w-4xl mx-auto'>
-        <Form />
-        </div>
-
+    <section className='bg-indigo-400 py-20 px-5'>
+            <div className='max-w-4xl mx-auto'>
+            <p>Formulario</p>
+            </div>
     </section>
     </>
   )
--- 4.contador-Calorias/src/data/categories.ts
+++
@@ -1,6 +0,0 @@
-import { Category } from "../types"
-
-export const categorias: Category[] = [
-    { id: 1, name: 'Comida'},
-    { id: 2, name: 'Ejercicio'}
-  ]
