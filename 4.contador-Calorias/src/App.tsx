8b1b34387fc9390e89b65846f490d43c1da58eb7
daniel mena <dmena044@gmail.com>
2025-01-21 13:15:08 -0600

    contador calorias primer merge

--- 4.contador-Calorias/src/App.tsx
+++ 4.contador-Calorias/src/App.tsx
@@ -1,23 +1,10 @@
 import './App.css'
-
+import { Form } from './components/Form'
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-    <header className='bg-indigo-500 py-3'>
-        <div className="max-w-4xl mx-auto flex justify-between">
-            <h1 className='text-center text-lg font-bold text-white uppercase'>
-                Contador de Calorias
-            </h1>
-        </div>
-    </header>
 
-    <section className='bg-indigo-400 py-20 px-5'>
-            <div className='max-w-4xl mx-auto'>
-            <p>Formulario</p>
-            </div>
-=======
     <header className='bg-indigo-400 py-3'>
         <div className='max-w-4xl mx-auto flex justify-between'>
             <h1 className='text-center text-lg font-bold uppercase text-white'>Contador de Calorias</h1>
@@ -29,7 +16,6 @@ function App() {
         <Form />
         </div>
 
->>>>>>> develop
     </section>
     </>
   )
---
+++ 4.contador-Calorias/src/data/categories.ts
@@ -0,0 +1,6 @@
+import { Category } from "../types"
+
+export const categorias: Category[] = [
+    { id: 1, name: 'Comida'},
+    { id: 2, name: 'Ejercicio'}
+  ]
