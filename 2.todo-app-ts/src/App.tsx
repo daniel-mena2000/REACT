0b21258166f596d7f24cb3ce8ddbc516ffb46594
daniel mena <dmena044@gmail.com>
2025-12-04 23:29:36 -0600

    proyecto todo-list con TS

--- 2.todo-app-ts/src/App.tsx
+++ 2.todo-app-ts/src/App.tsx
@@ -1,11 +1,36 @@
-
+import Form from './components/Form'
 import './App.css'
-
+import { infoList } from './data/lista'
+import { useEffect, useState } from 'react'
+import CartList from './components/CardList'
+import type { List } from './types'
+import type { ListBasic } from './types'
 function App() {
 
+    const [task, setTask] = useState<List[]>([])
+
+    useEffect(() => {
+        setTask(infoList)
+    },[])
+
+    function agregarTarea(info: ListBasic) {
+        setTask([...task, {
+            nombre: info.nombre,
+            descripcion: info.descripcion,
+            id: task.length + 1
+    }])
+    }
+    function eliminar(id: List['id']) {
+        setTask(task.filter(item => item.id !== id))
+    }
   return (
     <>
+        <h1>Lista de tareas</h1>
 
+    <div className="container">
+      <Form agregarTarea={agregarTarea} />
+      <CartList task={task} eliminar={eliminar}/>
+    </div>
     </>
   )
 }
