639682164bd79c2278d92ccb4050a242b0f9e16d
daniel mena <dmena044@gmail.com>
2026-02-13 23:59:03 -0600

    control de gastos 3 Funcionalidad del modal

--- 5.control-gastos-contextapi/src/components/ExpenseModal.tsx
+++ 5.control-gastos-contextapi/src/components/ExpenseModal.tsx
@@ -1,11 +1,45 @@
 import { categories } from "../db/categories"
+import DatePicker from 'react-date-picker'; //"npm i react-date-picker"
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css'; // instalamos primero "npm i react-calendar"
+import { useState } from "react";
+import type { DraftExpense } from "../types";
+import type { Value } from "react-calendar/dist/shared/types.js";
+
 
 type ExpenseModalProps = {
   open: boolean
   onClose: () => void
 }
 
+
+
 export default function ExpenseModal({ open, onClose }: ExpenseModalProps) {
+
+const [expense, setExpense] = useState<DraftExpense>({
+    amount: 0,
+    expenseName: '',
+    category: '',
+    date: new Date()
+})
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+//Verificamos si algun "name" de algun input es amount, para despues a ese convertirlo en numero
+    const isAmountField = ['amount'].includes(e.target.name)
+
+    setExpense({
+        ...expense,
+        [e.target.name]: isAmountField ? Number(e.target.value) : e.target.value
+    })
+}
+
+const handleChangeDate = (value: Value) => {
+    setExpense({
+        ...expense,
+        date: value
+    })
+}
+
   if (!open) return null
 
   return (
@@ -27,6 +61,8 @@ export default function ExpenseModal({ open, onClose }: ExpenseModalProps) {
             className="bg-slate-100 p-2"
             id="expenseName"
             name="expenseName"
+            value={expense.expenseName}
+            onChange={handleChange}
             />
         </div>
 
@@ -38,13 +74,15 @@ export default function ExpenseModal({ open, onClose }: ExpenseModalProps) {
             className="bg-slate-100 p-2"
             id="amount"
             name="amount"
+            value={expense.amount}
+            onChange={handleChange}
             />
        </div>
 
         <div className="flex flex-col gap-2 mt-2">
             <label htmlFor="amount" className="font-bold">Categoria: </label>
 
-            <select className="bg-slate-100 p-2" name="category" id="category">
+            <select className="bg-slate-100 p-2" name="category" id="category" value={expense.category} onChange={handleChange}>
                 <option value="">-- Seleccione --</option>
                 {categories.map(item => (
                     <option value={item.id} key={item.id}>
@@ -54,10 +92,14 @@ export default function ExpenseModal({ open, onClose }: ExpenseModalProps) {
             </select>
         </div>
 
-        <input
-          type="date"
-          className="border p-2 w-full mb-4"
-        />
+        <div className="flex flex-col gap-2 mt-2">
+        <label htmlFor="amount" className="font-bold">Fecha Gasto: </label>
+            <DatePicker
+                className="bg-sky-100 p-2 border-0"
+                value={expense.date}
+                onChange={handleChangeDate}
+            />
+       </div>
 
       <input type="submit" className="bg-sky-600 text-white px-4 py-2 rounded-lg w-full font-bold uppercase"
             value={'Registrar Gasto'}
